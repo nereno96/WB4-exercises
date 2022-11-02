3cd93c276aaf58e37e5e26a98aa86caa1c1d2be0
Najee Reno <nereno96@gmail.com>
2022-11-02 10:52:48 -0400

    added getMedian() funtion to avg_scores.js

--- ArrayScripts/avg_scores.js
+++ ArrayScripts/avg_scores.js
@@ -17,3 +17,36 @@ function getAverage(scores) {
 
 console.log(getAverage(myScores));
 console.log(getAverage(yourScores));
+
+function compareAscendingNumbers (a,b) {
+    if (a < b) {
+        return -1;
+    }
+    else if (a == b) {
+        return 0;
+    }
+    else {
+        return 1;
+    }
+}
+
+function getMedian(scores) {
+    let numScores = scores.length;
+    let sortedScores = [];
+    let median;
+    let upperBound = numScores - 1;
+    sortedScores = scores.sort(compareAscendingNumbers);
+    if (numScores % 2 == 1) {
+        median = sortedScores[upperBound / 2];
+        return median;
+    }
+    else {
+        median = (sortedScores[numScores / 2] + sortedScores[(numScores / 2) - 1]) / 2;
+        return median;
+    }
+}
+
+console.log(getMedian([ 80, 83, 86, 92, 100]));
+console.log(getMedian([ 80, 83, 86, 87, 92, 100]));
+console.log(getMedian(myScores))
+console.log(getMedian(yourScores))
